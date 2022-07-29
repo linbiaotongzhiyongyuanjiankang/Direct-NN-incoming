da6ef2b74c154e0f0a8aebd3591f5ca44da5fc96
CCC101 <101533240+linbiaotongzhiyongyuanjiankang@users.noreply.github.com>
2022-07-29 15:32:19 +0800

    Add files via upload

--- seekfind/__init__.py
+++ seekfind/__init__.py
@@ -23,7 +23,7 @@ def sep(sep, skf_record):
         sep_record = sep_record + [temp]
     return sep_record
 
-def check( former_two, former_one, current):
+def check(former_two, former_one, current):
     a = (former_one[0] - former_two[0])
     b = (former_one[1] - former_two[1])
     c = (current[0] - former_one[0])
@@ -41,7 +41,33 @@ def check( former_two, former_one, current):
 
 def possi_route(route_data, step):
     big_record = []
+    temp = route_data[step]
+    old_one = route_data[step - 1]
+    old_two = route_data[step - 2]
     if step + 1 == len(route_data):
-        temp = route_data[step]
-        for i in range(0, len(temp)):
-            current = temp[i]
+        for r in range(0, len(temp)):
+            current = temp[r]
+            for s in range(0, len(old_one)):
+                former_one = old_one[s]
+                for t in range(0, len(old_two)):
+                    former_two = old_two[t]
+                    if check(former_two, former_one, current) == True:
+                        big_record.append(current)
+    elif step == 2 or step >2 and step+1 < len(route_data):
+        for r in range(0, len(temp)):
+            current = temp[r]
+            for s in range(0, len(old_one)):
+                former_one = old_one[s]
+                for t in range(0, len(old_two)):
+                    former_two = old_two[t]
+                    if check(former_two, former_one, current) == True:
+                        for forward in possi_route(route_data, step+1):
+                            big_record.append(forward + [current])
+    else:
+        for r in range(0, len(temp)):
+            current = temp[r]
+            for forward in possi_route(route_data, step+1):
+                big_record.append(forward + [current])
+    return big_record
+
+
